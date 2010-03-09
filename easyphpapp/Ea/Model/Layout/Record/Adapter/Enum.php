624858d089b0656a2b3458de4f290d5c511b19c9
berlioz@nicematin.fr <berlioz@nicematin.fr@9035d796-9552-11dd-b10c-7173267903d1>
2010-03-09 12:19:12 +0000

    add enum mapping to model layout

---
+++ easyphpapp/Ea/Model/Layout/Record/Adapter/Enum.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * EasyPhpApp Framework
+ * A simple form application framework
+ * 
+ * @category    EasyPhpApp
+ * @package     Model
+ * @subpackage  Form
+ * @author      David Berlioz <berlioz@nicematin.fr>
+ * @version     0.4.4-20100309
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3
+ * @copyright   David Berlioz <berlioz@nicematin.fr>
+ */
+
+require_once 'Ea/Model/Layout/Record/Adapter/Interface.php';
+require_once 'Ea/Layout/Record/Adapter/Field.php';
+
+/**
+ * Text layout for column.
+ */
+class Ea_Model_Layout_Record_Adapter_Enum extends Ea_Layout_Record_Adapter_Field implements Ea_Model_Layout_Record_Adapter_Interface
+{
+	public function __construct($column, Ea_Model_Layout $model)
+	{
+		parent::__construct($column, $model->getColumnEnum($column));
+		$this->_filter=array($model, 'filterRecordValue');
+	}
+}
+
+?>
