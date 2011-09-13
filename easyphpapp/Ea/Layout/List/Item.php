e2213de835a505b0f5b0a68d8fc6fd61924ae3e8
quazardous <quazardous@9035d796-9552-11dd-b10c-7173267903d1>
2011-09-13 07:51:17 +0000

    add list layout

---
+++ easyphpapp/Ea/Layout/List/Item.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * EasyPhpApp Framework
+ * A simple form application framework
+ * 
+ * @category    EasyPhpApp
+ * @package     Layout
+ * @subpackage  List
+ * @author      David Berlioz <berlioz@nicematin.fr>
+ * @version     0.5.2-20110912
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3
+ * @copyright   David Berlioz <berlioz@nicematin.fr>
+ */
+
+require_once 'Ea/Layout/Container.php';
+
+/**
+ * List item layout class.
+ * 
+ * @see Ea_Layout_List
+ */
+class Ea_Layout_List_Item extends Ea_Layout_Container
+{
+	protected $_tag='li';
+	
+	/**
+	 * Table cell layout constructor.
+	 * 
+	 * @param array $config associative array of parameters
+	 *  
+	 */
+	public function __construct($config=null)
+	{
+		parent::__construct(null, $config);
+	}
+}
+
