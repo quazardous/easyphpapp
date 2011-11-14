df52d702525a9259e7a09e70a8e23dd5f254ca1d
quazardous <quazardous@9035d796-9552-11dd-b10c-7173267903d1>
2011-11-14 14:16:05 +0000

    add $Id:$

--- easyphpapp/Ea/Model/Data/Select.php
+++ easyphpapp/Ea/Model/Data/Select.php
@@ -7,7 +7,7 @@
  * @package     Model
  * @subpackage  Data
  * @author      David Berlioz <berlioz@nicematin.fr>
- * @version     0.4.6-20101007
+ * @version     $Id:$
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3
  * @copyright   David Berlioz <berlioz@nicematin.fr>
  */
