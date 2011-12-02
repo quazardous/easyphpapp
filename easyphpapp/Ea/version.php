e088494c047b9a5c972bd59181b1dfe335296a64
quazardous <quazardous@9035d796-9552-11dd-b10c-7173267903d1>
2011-12-02 08:00:39 +0000

    change version

--- easyphpapp/Ea/version.php
+++ easyphpapp/Ea/version.php
@@ -7,10 +7,10 @@
  * @package     Application
  * @subpackage  define
  * @author      David Berlioz <berlioz@nicematin.fr>
- * @version     $Id:$
+ * @version     $Id$
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3
  * @copyright   David Berlioz <berlioz@nicematin.fr>
  */
 
 // current version
-define('EA_VERSION', '0.5.3-pink');
+define('EA_VERSION', '0.5.4-pink');
